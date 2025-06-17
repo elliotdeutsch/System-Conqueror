3829ac02179b01ceff3409ec0b8a3f3907a8b708
elliotdeutsch <58427038+elliotdeutsch@users.noreply.github.com>
2025-06-17 18:58:42 +0200

    Delay timer start until game begins

--- Assets/Scripts/UI/GameSetupUI.cs
+++ Assets/Scripts/UI/GameSetupUI.cs
@@ -67,5 +67,10 @@ public class GameSetupUI : MonoBehaviour
         }
 
         galaxyManager.InitializeGalaxy();
+
+        if (GameTimer.Instance != null)
+        {
+            GameTimer.Instance.StartTimer();
+        }
     }
 }
